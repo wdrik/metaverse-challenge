fbc59e90fe1a119fe106c53e038f8d6dff35ae72
wdrik <w.drik@outlook.com>
2022-01-08 11:01:42 -0300

    feat: add change username component

--- components/Avatar.js
+++ components/Avatar.js
@@ -4,7 +4,14 @@ import { useMoralis } from 'react-moralis';
 function Avatar({ username, logoutOnPress }) {
   const { user, logout } = useMoralis();
 
-  return <Image src={`https://avatars.dicebars.com/api/pixel-art/${username || user.get('username')}`} />;
+  return (
+    <Image
+      className="rounded-full bg-black cursor-pointer hover:opacity-75"
+      src={`https://avatars.dicebear.com/api/pixel-art/${username || user.get('username')}.svg`}
+      layout="fill"
+      onClick={() => logoutOnPress && logout()}
+    />
+  );
 }
 
 export default Avatar;
---
+++ components/ChangeUsername.js
@@ -0,0 +1,25 @@
+import { useMoralis } from 'react-moralis';
+
+function ChangeUsername() {
+  const { setUserData, isUserUpdating, userError, user } = useMoralis();
+
+  const setUserName = () => {
+    const username = prompt(`Enter your new Username (current: ${user.getUsername()})`);
+
+    if (!username) return;
+
+    setUserData({
+      username,
+    });
+  };
+
+  return (
+    <div className="text-sm absolute top-5 right-5">
+      <button disabled={isUserUpdating} onClick={setUserName} className="hover:text-pink-700">
+        Change yout Username
+      </button>
+    </div>
+  );
+}
+
+export default ChangeUsername;
